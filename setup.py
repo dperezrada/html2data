631ce6906d4f021beba1b41868c175ed0044b213
Daniel Pérez Rada <dperezrada@gmail.com>
2011-11-24 22:41:32 -0300

    FIX: Problem when installing using easy_install

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ import os
 from setuptools import setup
 
 def read(fname):
+    print os.path.join(os.path.dirname(__file__), fname)
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "html2data",
-    version = "0.4.1",
+    version = "0.4.2",
     author = "Daniel Perez Rada",
     author_email = "dperezrada@gmail.com",
     description = ("A simple way to transform a HTML file or URL to structured data."),
@@ -16,6 +17,7 @@ setup(
     url = "https://github.com/dperezrada/html2data",
     packages=['html2data', 'tests'],
     long_description=read('README.rst'),
+    include_package_data=True,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
